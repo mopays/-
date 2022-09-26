0cb8cf1e7be3516cb0e304327ee05847cfcc9748
mopays <75215491+mopays@users.noreply.github.com>
2022-09-26 16:10:41 +0700

    Update connect.php

--- assets/config/connect.php
+++ assets/config/connect.php
@@ -5,11 +5,9 @@
     $db_password = '';
     $db_name = 'fishhead';
     
-    try {
+   
 
-    $db = new \PDO("mysqli:host={$db_host};dbname={$db_name}",$db_user, $db_password);
+    $db = new PDO("mysqli:host={$db_host};dbname={$db_name}",$db_user, $db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    print $e->getMessage() . "\n";
-}
+
     ?>
